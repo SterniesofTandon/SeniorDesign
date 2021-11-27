e5a18dbbaf2d7ccdaeba72c7320a6cbd71ab24aa
lmenezesl <luiza.menezes@nyu.edu>
2021-11-27 16:49:44 -0500

    Test 1, data dump from phpmyadmin

--- create_dbs.sql
+++ create_dbs.sql
@@ -1,6 +1,15 @@
+'''
 CREATE TABLE user(
 	username varchar(50),
 	password varchar(50),
+    anon_code` varchar(50),
+    '''
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `phone_number` varchar(50) DEFAULT NULL,
+  `card_number` varchar(50) DEFAULT NULL
+  '''
 	PRIMARY KEY(username)
 );
 
@@ -10,3 +19,56 @@ CREATE TABLE blog(
 	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
 	FOREIGN KEY (username) REFERENCES user(username)
 );
+'''
+
+
+-- phpMyAdmin SQL Dump
+-- version 4.9.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Nov 27, 2021 at 09:47 PM
+-- Server version: 5.7.32
+-- PHP Version: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `flaskdemo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `anon_code` varchar(50) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `phone_number` varchar(50) DEFAULT NULL,
+  `card_number` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`username`, `password`, `anon_code`, `first_name`, `last_name`, `address`, `phone_number`, `card_number`) VALUES
+('customer1', 'vibe', NULL, NULL, NULL, NULL, NULL, NULL),
+('luizaTest', 'syrup', NULL, NULL, NULL, NULL, NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`username`);
